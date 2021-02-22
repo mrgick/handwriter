d82c71ed31522e0dc67a83ba31fa702ecb115e99
mrgick <45306973+mrgick@users.noreply.github.com>
2021-02-22 11:33:33 +0300

    change to english comments

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from PIL import Image
 import random
 import os
 
-#глобальный путь
+#global dir path
 path = os.path.abspath(os.curdir)+'/'
 
-#символы, которые я нарисовал
-symbols = "abcdefghijklmnopqrstuvwxyz0123456789:;,'<>=*().-+/_абввгдеёжзиклмопрстуфхцчшщъыьэюя"
-
-#чтение файла
+#create array from lines from file
 def read_file(file_name):
 	f = open(file_name,'r')
 	data = f.read()
@@ -16,8 +13,8 @@ def read_file(file_name):
 	data = data.split('\n')
 	return data
 
-#вставка буквы в изображение
-def insert_letter(img_source,symbol_name,px,py):
+#inser one symbol in image
+def insert_symbol(img_source,symbol_name,px,py):
 	if symbol_name =='.':
 		symbol_name = 'point'
 	if symbol_name == '/':
@@ -25,7 +22,7 @@ def insert_letter(img_source,symbol_name,px,py):
 	path_symbols = path + 'symbols/' + symbol_name
 	if os.path.exists(path_symbols) == True:
 		
-		#определяем кол-во вариаций и выбираем одну
+		#chose one symbol
 		num = 1
 		path2 =path_symbols+'/'+str(num)+'.png'
 		while os.path.exists(path2) == True:
@@ -35,16 +32,16 @@ def insert_letter(img_source,symbol_name,px,py):
 
 		img_letter = Image.open(path_symbols+'/'+str(num)+'.png')
 
-		# изменяем размер
+		#change size
 		rresize = int(random.uniform(-3,3))
 		img_letter = img_letter.resize((img_letter.size[0]+rresize, img_letter.size[1]+rresize), Image.ANTIALIAS)
 		px_img_size = img_letter.size[0]		
 
-		#поворот
+		#rotate
 		rrot = random.uniform(-3,3)
 		img_letter=img_letter.rotate(rrot,Image.BILINEAR,expand=True)
 
-		#вставляем в картину	
+		#insert in image	
 		px = px+ int(random.uniform(2,6))
 		rpy = int(random.uniform(0,4))
 		img_source.alpha_composite(img_letter, (px, py+rpy))
@@ -56,7 +53,7 @@ def insert_letter(img_source,symbol_name,px,py):
 
 
 
-#создание строки с буквами
+#creating line with symbols
 def line_create(symbols_str,paper='clear'):
 	px = 0
 	py = 0
@@ -66,44 +63,48 @@ def line_create(symbols_str,paper='clear'):
 		if symbols_str[i]!=" " and symbols_str[i]!="\t":
 			symbol = symbols_str[i].lower()
 
-			#производим замену некоторых символов
+			#change some symbols
 			if symbol=='┌' or symbol=='┬' or symbol=='┐' or symbol=='└' or symbol=='┴' or symbol=='┘' or symbol=='─':
 				symbol = '-'
 			elif symbol=='│' or symbol=='├' or symbol=='|' or symbol=='┼' or symbol=='┤': 
 				symbol='l'
 
-			img_line,px = insert_letter(img_line,symbol,px,py) 
+			img_line,px = insert_symbol(img_line,symbol,px,py) 
 			px = px
 		else:
-			px = px+25+int(random.uniform(-10,10))
+			px = px+20+int(random.uniform(-10,10))
 	return img_line
 
 
-#представление печатного текста в рукописном
+#show array with text in image
 def array_to_png(arr):
-	source_img = Image.open(path+'background/A35.png')
+	source_img = Image.open(path+'background/clear.png')
 	py=159
 	for j in range(len(arr)):
-		px = 10+int(random.uniform(-10,10))
+		px = 15+int(random.uniform(-10,10))
 		img_line = line_create(arr[j],'clear')
 		source_img.alpha_composite(img_line, (px, py))
 		px = px
 
-		#py = py + 59 #one 
+		#py = py + 59 #one square
 		if j == 13:
 			py = py+118+59
 		else:
-			py = py + 118 #two
+			py = py + 118 #two square
 	return source_img
 
 
 def main():
 
-	#s = read_file('path_to_your_file')
+	#symbols that I draw
+	symbols = "abcdefghijklmnopqrstuvwxyz0123456789:;,'<>=*().-+/_абввгдеёжзиклмопрстуфхцчшщъыьэюя"
+
+	#s = read_file('path to file')
 	s = [symbols,symbols,symbols]
 	source_img = array_to_png(s)
 
-	#source_img = line_create(symbols)
+	#word = "somethin"
+	#source_img = line_create(word)
 	
 	source_img.show()
 	source_img.save("output/output.png")
